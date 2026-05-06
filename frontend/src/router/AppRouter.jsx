57bac07f2ae389fe3991c4c0f01d86a2cb9d3770
YARAGANI DURGA DHANUSH <ydurgadhanush@gmail.com>
2026-05-06 21:09:50 +0530

    Refactor AppRouter to remove Layout component

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,28 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "../components/Layout";
+import { Routes, Route } from "react-router-dom";
 
+// Import all your pages
 import Home from "../pages/Home";
-import Login from "../pages/Auth/Login";
-import Register from "../pages/Auth/Register";
 import Detector from "../pages/Detector";
 import Geo from "../pages/Geo";
 import Bookmarks from "../pages/Bookmarks";
 import Profile from "../pages/Profile";
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/detector" element={<Detector />} />
-          <Route path="/geo" element={<Geo />} />
-          <Route path="/bookmarks" element={<Bookmarks />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Routes>
+      {/* The Launchpad */}
+      <Route path="/" element={<Home />} />
+      
+      {/* The Two Decoupled AI Engines */}
+      <Route path="/detector" element={<Detector />} />
+      <Route path="/geo" element={<Geo />} />
+      
+      {/* User Management & History */}
+      <Route path="/bookmarks" element={<Bookmarks />} />
+      <Route path="/profile" element={<Profile />} />
+      
+      {/* Authentication */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
